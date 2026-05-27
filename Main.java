63ef75438ba5c9d2320cd2108a2a0ec43a2ffc54
Pratik Agrawal <pratikagrawal787@gmail.com>
2026-05-27 21:09:43 +0530

    prac. 1

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+  public static void main(String[] args) {
+
+
+         	System.out.println("Hello World");
+                System.out.println("Name:- Pratik agrawal");
+                System.out.println("Sap ID:- 500123XXX");
+                System.out.println("Batch:- B2");
+                System.out.println("Roll No:- R2142231XXX");
+                             
+  }
+}
