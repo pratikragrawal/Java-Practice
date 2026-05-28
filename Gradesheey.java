23e76b6d46e12637b7b498d9f26dd8d28ef7aa6a
Pratik Agrawal <pratikagrawal787@gmail.com>
2026-05-28 15:19:26 +0530

    prac. 7

---
+++ Gradesheey.java
@@ -0,0 +1,18 @@
+class Gradesheet {
+    public static void main(String[] args) {
+        System.out.println("***************************************");
+        System.out.println("*             GRADESHEET          *");
+        System.out.println("***************************************\n");
+
+        System.out.println("Name\t\tRoll Number\tSAP ID\t\tResult");
+        System.out.println("---------------------------------------------------------");
+
+        System.out.println("Pratik\t\tR2142\t\t500123XXX\t\tPass");
+        
+
+        System.out.println("\n***************************************");
+        System.out.println("*               END OF REPORT       *");
+        System.out.println("***************************************");
+    }
+}
+
